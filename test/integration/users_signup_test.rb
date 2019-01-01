d97291ec60f50faa956fa25a3f28fad380596676
Lorenzo Ciglioni <lorenzo.ciglioni@gmail.com>
2019-01-01 20:03:46 +0100

    Add more tests for signup errors and handles the redirect for new user creation

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -5,15 +5,20 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
 	test "Invalid signup information" do
 		get signup_path
 		assert_no_difference "User.count" do
-			post signup_path, params: { user: {  name: "",
-																					email: "user@invalid",
-																					password: "xxx",
-																					password_confirmation: "yyy" } }
+			post signup_path, params: { user: { name: "", email: "user@invalid", password: "xxx", password_confirmation: "yyy" } }
 		end
 		assert_template 'users/new'
 		assert_select 'div#error_explanation'
     assert_select 'div.field_with_errors'
 		assert_select 'form[action="/signup"]'
 	end
+
+	# test "Valid signup information" do
+	# 	get signup_path
+	# 	assert_difference "User.count" do
+	# 		post signup_path, params: { user: { name: "Foo Baz", email: "foobaz@example.com", password: "111111", password_confirmation: "11111" } }
+	# 	end
+	# 	assert_template user_path
+	# end
 
 end
