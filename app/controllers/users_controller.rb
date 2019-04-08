0cdd8555d65b89c8ac0b538349d608331d7087e2
Lorenzo Ciglioni <lorenzo.ciglioni@gmail.com>
2019-04-08 08:07:17 +0200

    Add redirect for non-logged and non-correct users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
 	before_action :logged_in_user, only: [:edit, :update]
+	before_action :correct_user, only: [:edit, :update]
 
   def new
 		@user = User.new
@@ -34,12 +35,18 @@ class UsersController < ApplicationController
 		@user = User.find(params[:id])
 	end
 
-	# Confirm a logged-in user.
+	# Confirms a logged-in user.
 	def logged_in_user
 		unless logged_in?
 			flash[:danger] = "Please log in."
 			redirect_to login_url
 		end
+	end
+
+	# Confirms the correct user
+	def correct_user
+		@user = User.find(params[:id])
+		redirect_to(root_url) unless current_user?(@user)
 	end
 
 	private
